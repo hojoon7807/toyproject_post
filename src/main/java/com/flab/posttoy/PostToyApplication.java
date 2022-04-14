703c9c58bac830541722d4d12a73bd344631a869
jwahoonKim <kjhoon0330@snu.ac.kr>
2022-04-14 15:07:28 +0900

    Initial commit

---
+++ src/main/java/com/flab/posttoy/PostToyApplication.java
@@ -0,0 +1,13 @@
+package com.flab.posttoy;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PostToyApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PostToyApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/flab/posttoy/PostToyApplicationTests.java
@@ -0,0 +1,13 @@
+package com.flab.posttoy;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PostToyApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
