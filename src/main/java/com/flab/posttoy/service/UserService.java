441ff66807b05e4e5156d9207fb9e9996d31e5d5
hojoon <ujuj0202@gmail.com>
2022-04-16 00:03:51 +0900

    FEAT: UserService 기능 추가
    getMyPostList 나중에 구현

---
+++ src/main/java/com/flab/posttoy/service/UserService.java
@@ -0,0 +1,47 @@
+package com.flab.posttoy.service;
+
+import com.flab.posttoy.domain.Post;
+import com.flab.posttoy.domain.User;
+import com.flab.posttoy.exception.NotFoundUserException;
+import com.flab.posttoy.repository.post.PostRepository;
+import com.flab.posttoy.repository.user.UserMemoryRepository;
+import com.flab.posttoy.repository.user.UserRepository;
+import org.springframework.stereotype.Service;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@Service
+public class UserService {
+    private final UserRepository userRepository;
+    private final PostRepository postRepository;
+
+    public UserService(UserRepository userRepository, PostRepository postRepository) {
+        this.userRepository = userRepository;
+        this.postRepository = postRepository;
+    }
+
+    /**
+     * 회원가입
+     */
+    public long signUp(User user){
+        userRepository.save(user);
+        return user.getId();
+    }
+
+    /**
+     * 로그인
+     */
+    public long signIn(String username) {
+        User user = userRepository.findByName(username).orElseThrow(()-> new NotFoundUserException("회원 정보가 존재하지 않습니다."));
+        return user.getId();
+    }
+
+    /**
+     * 본인 게시글 조회
+     */
+    public List<Post> getMyPostList(){
+        return new ArrayList<Post>();
+    }
+
+}
