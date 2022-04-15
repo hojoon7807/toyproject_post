719f3e5cf428f845074f71293dff840c3ba8ddac
hojoon <ujuj0202@gmail.com>
2022-04-16 03:15:41 +0900

    FEAT: 간단한 가입 기능 추가

---
+++ src/main/java/com/flab/posttoy/controller/UserController.java
@@ -0,0 +1,27 @@
+package com.flab.posttoy.controller;
+
+import com.flab.posttoy.domain.User;
+import com.flab.posttoy.dto.SignUpDTO;
+import com.flab.posttoy.service.UserService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
+
+import javax.validation.Valid;
+
+@RestController
+@RequiredArgsConstructor
+public class UserController {
+    private final UserService userService;
+
+    // ExceptionHandler 추가 예정
+    @PostMapping("/users")
+    public ResponseEntity<User> create(@RequestBody @Valid SignUpDTO signUpDTO){
+        User user = signUpDTO.toEntity();
+        userService.signUp(user);
+        return ResponseEntity.ok().body(user);
+    }
+}
