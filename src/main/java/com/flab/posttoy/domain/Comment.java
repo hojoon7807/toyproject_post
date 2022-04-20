29a5b9530a2ce88da7c6c2b77b82e56053bcfa72
hojoon <ujuj0202@gmail.com>
2022-04-21 03:58:59 +0900

    CHORE: 도메인에 Builder 적용
    도메인 엔티티는 불변객체를 유지해야되기 때문에 setter보단 Builder를 사용하는 방식으로 했다.

--- src/main/java/com/flab/posttoy/domain/Comment.java
+++ src/main/java/com/flab/posttoy/domain/Comment.java
@@ -1,8 +1,11 @@
 package com.flab.posttoy.domain;
 
+import lombok.Builder;
 import lombok.Getter;
 
+// 불변객체를 유지하기 위해 setter 대신 builder
 @Getter
+@Builder
 public class Comment {
     private Long id;
     private Long userId;
