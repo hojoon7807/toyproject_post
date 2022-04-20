1ef3a4f65becf348dd53cfbc05196f8602642d24
hojoon <ujuj0202@gmail.com>
2022-04-21 04:01:55 +0900

    FEAT: service와 repository 사이의 데이터 변환을 위한 DTO 추가
    mapper로 엔티티를 변환하기 위한 dto를 추가했다.

---
+++ src/main/java/com/flab/posttoy/dto/UserDTO.java
@@ -0,0 +1,12 @@
+package com.flab.posttoy.dto;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+public class UserDTO {
+    private Long id;
+    private String username;
+    private String password;
+}
