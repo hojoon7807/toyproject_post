1c39f195b2bd4840b9fdefdd9b3978cf78966798
hojoon <ujuj0202@gmail.com>
2022-04-22 17:27:50 +0900

    FEAT: CommentController 구현
    Remove의 경우 댓글이 삭제되었다는 응답 문자열을 리턴하는데 이것도 Response 객체를 만들어줘야될까??

--- src/main/java/com/flab/posttoy/web/controller/CommentController.java
+++ src/main/java/com/flab/posttoy/web/controller/CommentController.java
@@ -1,4 +1,43 @@
 package com.flab.posttoy.web.controller;
 
+import com.flab.posttoy.dto.CommentDTO;
+import com.flab.posttoy.dto.mapper.CommentMapper;
+import com.flab.posttoy.service.ICommentService;
+import com.flab.posttoy.web.dto.mapper.WebCommentMapper;
+import com.flab.posttoy.web.dto.request.RequestCommentDTO;
+import com.flab.posttoy.web.dto.response.ResponseCommentDTO;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import javax.validation.Valid;
+import java.net.URI;
+
+@RestController
+@RequiredArgsConstructor
 public class CommentController {
+
+    private final ICommentService commentService;
+    private final WebCommentMapper commentMapper;
+
+    @PostMapping("/posts/{postId}/comments")
+    public ResponseEntity<ResponseCommentDTO> commentSave(@RequestBody @Valid RequestCommentDTO requestCommentDTO){
+        CommentDTO commentDTO = commentService.addComment(commentMapper.toCommentDto(requestCommentDTO));
+        ResponseCommentDTO responseCommentDTO = commentMapper.toResponseCommentDto(commentDTO);
+        return ResponseEntity.created(URI.create("/posts/"+responseCommentDTO.getPostId())).body(responseCommentDTO);
+    }
+
+    @PatchMapping("/posts/{postId}/comments/{commentId}")
+    public ResponseEntity<ResponseCommentDTO> commentModify(@RequestBody RequestCommentDTO requestCommentDTO, @PathVariable Long commentId) {
+        CommentDTO commentDTO = commentService.modifyComment(commentMapper.toUpdateCommentDto(requestCommentDTO), commentId);
+        ResponseCommentDTO responseCommentDTO = commentMapper.toResponseCommentDto(commentDTO);
+        return new ResponseEntity<>(responseCommentDTO, HttpStatus.OK);
+    }
+
+    @DeleteMapping("/posts/{postId}/comments/{commentId}")
+    public ResponseEntity<String> commentRemove(@PathVariable Long commentId) {
+        commentService.removeComment(commentId);
+        return new ResponseEntity<>("댓글이 삭제되었습니다.", HttpStatus.OK);
+    }
 }
