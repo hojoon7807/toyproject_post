54c9213202327726ebdf05b6e20926f7bc4dd130
hojoon <ujuj0202@gmail.com>
2022-04-22 17:25:39 +0900

    CHORE: ICommentService의 추상메서드 파라미터, 반환 타입 수정

--- src/main/java/com/flab/posttoy/service/ICommentService.java
+++ src/main/java/com/flab/posttoy/service/ICommentService.java
@@ -1,11 +1,13 @@
 package com.flab.posttoy.service;
 
 import com.flab.posttoy.domain.Comment;
+import com.flab.posttoy.dto.CommentDTO;
+import com.flab.posttoy.dto.UpdateCommentDTO;
 
 public interface ICommentService {
-    Comment addComment(Comment comment);
+    CommentDTO addComment(CommentDTO commentDTO);
 
-    Comment modifyComment(Comment comment, Long id);
+    CommentDTO modifyComment(UpdateCommentDTO updateCommentDTO, Long id);
 
     void removeComment(Long id);
 }
