daf89dd57818851a4b6ee4226ca67abc22062427
hojoon <ujuj0202@gmail.com>
2022-04-22 17:37:12 +0900

    FEAT: 응답에러와 GlobalExceptionHandler 구현
    블로그를 참고해서 작성해봤습니다

---
+++ src/main/java/com/flab/posttoy/web/exception/advice/GlobalExceptionHandler.java
@@ -0,0 +1,23 @@
+package com.flab.posttoy.web.exception.advice;
+
+import com.flab.posttoy.exception.comment.CommentNotFoundException;
+import com.flab.posttoy.web.exception.ErrorResponse;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.MethodArgumentNotValidException;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+@RestControllerAdvice
+public class GlobalExceptionHandler {
+    @ExceptionHandler(CommentNotFoundException.class)
+    public ResponseEntity<ErrorResponse> handleCommentNotFoundException(CommentNotFoundException e){
+        return new ResponseEntity<>(ErrorResponse.of(e.getErrorCode(),e.getMessage()), HttpStatus.NOT_FOUND);
+    }
+
+    @ExceptionHandler(MethodArgumentNotValidException.class)
+    public ResponseEntity<ErrorResponse> handleMethodArgumentNotValidException(MethodArgumentNotValidException e){
+        System.out.println("e.getModel(). = " + e.getModel());
+        return new ResponseEntity<>(ErrorResponse.of(e), HttpStatus.BAD_REQUEST);
+    }
+}
