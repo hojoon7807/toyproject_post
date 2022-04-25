ef23a7f9b4644a194b5cc6be7a9b78607792f0a3
hojoon <ujuj0202@gmail.com>
2022-04-26 01:12:19 +0900

    CHORE: service에 위치한 dto 패키지명 entity 변경

--- src/main/java/com/flab/posttoy/entity/UserEntity.java
+++ src/main/java/com/flab/posttoy/entity/UserEntity.java
@@ -1,11 +1,11 @@
-package com.flab.posttoy.dto;
+package com.flab.posttoy.entity;
 
 import lombok.Getter;
 import lombok.Setter;
 
 @Getter
 @Setter
-public class UserDTO {
+public class UserEntity {
     private Long id;
     private String username;
     private String password;
