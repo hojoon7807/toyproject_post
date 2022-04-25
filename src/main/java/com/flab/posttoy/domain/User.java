a6012736835249b37ae17f014580c3d243ac8364
hojoon <ujuj0202@gmail.com>
2022-04-26 01:20:04 +0900

    CHORE: User 도메인 빌더 삭제, 생성자, toString 추가

--- src/main/java/com/flab/posttoy/domain/User.java
+++ src/main/java/com/flab/posttoy/domain/User.java
@@ -2,16 +2,27 @@ package com.flab.posttoy.domain;
 
 import lombok.Builder;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 
 // 불변객체를 유지하기 위해 setter 대신 builder
 @Getter
-@Builder
 public class User {
     private Long id;
     private String username;
     private String password;
 
-    public void setId(Long id) {
+    public User(Long id, String username, String password) {
         this.id = id;
+        this.username = username;
+        this.password = password;
+    }
+
+    @Override
+    public String toString() {
+        return "User{" +
+                "id=" + id +
+                ", username='" + username + '\'' +
+                ", password='" + password + '\'' +
+                '}';
     }
 }
