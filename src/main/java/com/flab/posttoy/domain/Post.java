43a9a82f4c52d3cdff070fa4a283019e0dca91b2
hojoon <ujuj0202@gmail.com>
2022-04-26 21:38:57 +0900

    CHORE: Post 도메인 분리

--- src/main/java/com/flab/posttoy/domain/Post.java
+++ src/main/java/com/flab/posttoy/domain/Post.java
@@ -3,16 +3,14 @@ package com.flab.posttoy.domain;
 import lombok.Builder;
 import lombok.Getter;
 
+
 // 불변객체를 유지하기 위해 setter 대신 builder
 @Getter
 @Builder
 public class Post {
-    private Long id;
+    @Builder.Default
+    private Long id = Long.valueOf(0L);
     private Long userId;
     private String title;
     private String content;
-
-    public void setId(Long id) {
-        this.id = id;
-    }
 }
