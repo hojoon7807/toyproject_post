1b710599a6a24c1785cd74fde26fc5e3b9e2a48a
hojoon <ujuj0202@gmail.com>
2022-04-29 16:20:18 +0900

    refactor: 생성자 추가

--- src/main/java/com/flab/posttoy/repository/user/UserEntity.java
+++ src/main/java/com/flab/posttoy/repository/user/UserEntity.java
@@ -9,4 +9,9 @@ public class UserEntity {
     private Long id;
     private String username;
     private String password;
+
+    public UserEntity(String username, String password) {
+        this.username = username;
+        this.password = password;
+    }
 }
