dd9daeeaaed7803a3090cff22bc6bcdbbe29a1ce
hojoon <ujuj0202@gmail.com>
2022-04-29 16:16:37 +0900

    refactor: UserController  RequestBody, valid 어노테이션 추가

--- src/main/java/com/flab/posttoy/web/controller/UserController.java
+++ src/main/java/com/flab/posttoy/web/controller/UserController.java
@@ -2,25 +2,26 @@ package com.flab.posttoy.web.controller;
 
 import com.flab.posttoy.domain.User;
 import com.flab.posttoy.service.UserService;
-import com.flab.posttoy.web.dto.request.RequestUserDTO;
-import com.flab.posttoy.web.dto.response.ResponseUserDTO;
-import com.flab.posttoy.web.mapper.WebUserMapper;
+import com.flab.posttoy.web.dto.request.CreateUserRequest;
+import com.flab.posttoy.web.dto.response.UserResponseDTO;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
+
+import javax.validation.Valid;
 
 @RestController
 @RequiredArgsConstructor
 public class UserController {
 
     private final UserService userService;
-    private final WebUserMapper userMapper;
 
     @PostMapping("/users")
-    public ResponseEntity<ResponseUserDTO> userSave(RequestUserDTO requestUser){
-        User user = userService.addUser(userMapper.toUser(requestUser));
-        return new ResponseEntity<>(userMapper.toResponseUser(user), HttpStatus.CREATED);
+    public ResponseEntity<UserResponseDTO> userSave(@RequestBody @Valid CreateUserRequest createUserRequest){
+        User user = userService.addUser(createUserRequest.toCommand());
+        return new ResponseEntity<>(UserResponseDTO.from(user), HttpStatus.CREATED);
     }
 }
