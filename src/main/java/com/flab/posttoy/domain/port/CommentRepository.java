da802c93fbff926ee6b2ae40a56bf6a0e109d069
hojoon <ujuj0202@gmail.com>
2022-04-30 01:28:57 +0900

    refactor: repository 파라미터, 리턴 타입 수정

--- src/main/java/com/flab/posttoy/domain/port/CommentRepository.java
+++ src/main/java/com/flab/posttoy/domain/port/CommentRepository.java
@@ -1,17 +1,18 @@
 package com.flab.posttoy.domain.port;
 
 import com.flab.posttoy.domain.Comment;
+import com.flab.posttoy.repository.comment.CommentEntity;
 
 import java.util.List;
 import java.util.Optional;
 
 public interface CommentRepository {
-    Comment insert(Comment comment);
-    Comment update(Comment comment);
+    CommentEntity insert(CommentEntity comment);
+    CommentEntity update(CommentEntity comment);
     void delete(Long id);
-    Optional<Comment> selectById(Long id);
-    List<Comment> selectByPostId(Long postId);
-    List<Comment> selectAll();
+    Optional<CommentEntity> selectById(Long id);
+    List<CommentEntity> selectByPostId(Long postId);
+    List<CommentEntity> selectAll();
 
     void clearStore();
 
