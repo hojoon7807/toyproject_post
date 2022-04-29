765ed667dac5e3c6232c20f3deb72a89728b98d3
hojoon <ujuj0202@gmail.com>
2022-04-30 02:38:16 +0900

    feat: postDatail 응답을 위한 dto 추가
    toPostDto, toCommentDtoList 로직을 컨트롤러에서 실행을 해야되는건가 고민됩니다.

--- src/main/java/com/flab/posttoy/web/dto/response/PostDetailResponseDTO.java
+++ src/main/java/com/flab/posttoy/web/dto/response/PostDetailResponseDTO.java
@@ -1,15 +1,38 @@
 package com.flab.posttoy.web.dto.response;
 
+import com.fasterxml.jackson.databind.PropertyNamingStrategies;
+import com.fasterxml.jackson.databind.annotation.JsonNaming;
 import com.flab.posttoy.domain.Comment;
 import com.flab.posttoy.domain.Post;
+import com.flab.posttoy.domain.PostDetail;
 import lombok.Getter;
-import lombok.Setter;
 
 import java.util.List;
+import java.util.stream.Collectors;
 
 @Getter
-@Setter
+@JsonNaming(PropertyNamingStrategies.SnakeCaseStrategy.class)
 public class PostDetailResponseDTO {
-    private Post post;
-    private List<Comment> commentList;
+    private PostResponseDTO post;
+    private List<CommentResponseDTO> commentList;
+
+    private PostDetailResponseDTO(PostResponseDTO post, List<CommentResponseDTO> commentList) {
+        this.post = post;
+        this.commentList = commentList;
+    }
+
+    public static PostDetailResponseDTO from(PostDetail postDetail) {
+        return new PostDetailResponseDTO(toPostDto(postDetail.getPost()),
+                toCommentDtoList(postDetail.getCommentList()));
+    }
+
+    private static PostResponseDTO toPostDto(Post post) {
+        return PostResponseDTO.from(post);
+    }
+
+    private static List<CommentResponseDTO> toCommentDtoList(List<Comment> commentList) {
+        return commentList.stream()
+                .map(CommentResponseDTO::from)
+                .collect(Collectors.toList());
+    }
 }
