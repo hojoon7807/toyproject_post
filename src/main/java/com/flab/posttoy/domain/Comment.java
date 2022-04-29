38287ed71031aa7a12085eccb3dd2853b21aa587
hojoon <ujuj0202@gmail.com>
2022-04-30 01:26:48 +0900

    refactor: Comment 클래스 로직 삭제, 빌더 추가

--- src/main/java/com/flab/posttoy/domain/Comment.java
+++ src/main/java/com/flab/posttoy/domain/Comment.java
@@ -5,22 +5,17 @@ import lombok.Getter;
 
 // 불변객체를 유지하기 위해 setter 대신 builder
 @Getter
-@Builder
 public class Comment {
     private Long id;
     private Long userId;
     private Long postId;
     private String content;
 
-    public void changeComment(String content) {
-        if(isValidData(content)) this.content = content;
-    }
-
-    private boolean isValidData(String data){
-        return data == null ? true : false;
-    }
-
-    public void setId(Long id) {
+    @Builder
+    public Comment(Long id, Long userId, Long postId, String content) {
         this.id = id;
+        this.userId = userId;
+        this.postId = postId;
+        this.content = content;
     }
 }
