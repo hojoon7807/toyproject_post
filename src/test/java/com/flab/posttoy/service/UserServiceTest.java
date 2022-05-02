952925f98e7a2ede5bc563e3fc71d6259a2b3f7f
hojoon <ujuj0202@gmail.com>
2022-05-02 21:45:22 +0900

    test: UserService 테스트 코드 작성

--- src/test/java/com/flab/posttoy/service/UserServiceTest.java
+++ src/test/java/com/flab/posttoy/service/UserServiceTest.java
@@ -1,4 +1,69 @@
 package com.flab.posttoy.service;
 
+import com.flab.posttoy.domain.User;
+import com.flab.posttoy.domain.port.UserRepository;
+import com.flab.posttoy.exception.user.DuplicatedUserException;
+import com.flab.posttoy.repository.user.UserEntity;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
+import org.springframework.test.util.ReflectionTestUtils;
+
+import java.util.Optional;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
+import static org.junit.jupiter.api.Assertions.assertAll;
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.Mockito.*;
+
+@ExtendWith(MockitoExtension.class)
 public class UserServiceTest {
+
+    @Mock
+    private UserRepository userRepository;
+
+    @InjectMocks
+    private UserService userService;
+
+    @Test
+    @DisplayName("회원가입")
+    void addUser(){
+        CreateUserCommand createUserCommand = new CreateUserCommand("user1", "1234");
+        UserEntity userEntity = createUserCommand.toEntity();
+        Long fakeId = 1L;
+        ReflectionTestUtils.setField(userEntity, "id", fakeId);
+        when(userRepository.insert(any())).thenReturn(userEntity);
+        when(userRepository.selectByName(any())).thenReturn(Optional.empty());
+
+        User user = userService.addUser(createUserCommand);
+
+        assertAll(
+                () -> assertThat(user).isInstanceOf(User.class),
+                () -> assertThat(user.getId()).isEqualTo(1L),
+                () -> assertThat(user.getUsername()).isEqualTo("user1"),
+                () -> assertThat(user.getPassword()).isEqualTo("1234")
+        );
+
+        verify(userRepository, times(1)).insert(any(UserEntity.class));
+        verify(userRepository, times(1)).selectByName(any(String.class));
+    }
+
+    @Test
+    @DisplayName("중복된 이름의 회원이 존재하면 회원가입 실패")
+    void addUser_duplicatedUser(){
+        CreateUserCommand createUserCommand = new CreateUserCommand("user1", "1234");
+        UserEntity userEntity = createUserCommand.toEntity();
+        when(userRepository.selectByName(any())).thenReturn(Optional.of(userEntity));
+
+        assertThatThrownBy(() -> userService.addUser(createUserCommand))
+                .isInstanceOf(DuplicatedUserException.class)
+                .hasMessage("회원이 이미 존재합니다");
+
+        verify(userRepository, times(0)).insert(any(UserEntity.class));
+        verify(userRepository, times(1)).selectByName(any(String.class));
+    }
 }
