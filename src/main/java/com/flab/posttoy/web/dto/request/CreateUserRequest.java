7013b919623e5c11816e564d30531bfddac5465b
hojoon <ujuj0202@gmail.com>
2022-05-03 03:53:24 +0900

    test: UserController test 코드 작성

--- src/main/java/com/flab/posttoy/web/dto/request/CreateUserRequest.java
+++ src/main/java/com/flab/posttoy/web/dto/request/CreateUserRequest.java
@@ -3,11 +3,13 @@ package com.flab.posttoy.web.dto.request;
 import com.flab.posttoy.service.CreateUserCommand;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 
 import javax.validation.constraints.NotEmpty;
 
 @Getter
 @AllArgsConstructor
+@NoArgsConstructor
 public class CreateUserRequest {
     @NotEmpty
     private String username;
