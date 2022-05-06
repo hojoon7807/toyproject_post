8d7046b7bbf04045133982ed90173899f86b1759
hojoon <ujuj0202@gmail.com>
2022-05-06 20:01:10 +0900

    refactor: 생성자 어노테이션 추가

--- src/main/java/com/flab/posttoy/web/dto/request/CreateCommentRequest.java
+++ src/main/java/com/flab/posttoy/web/dto/request/CreateCommentRequest.java
@@ -3,13 +3,17 @@ package com.flab.posttoy.web.dto.request;
 import com.fasterxml.jackson.databind.PropertyNamingStrategies;
 import com.fasterxml.jackson.databind.annotation.JsonNaming;
 import com.flab.posttoy.service.CreateCommentCommand;
+import lombok.AllArgsConstructor;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 
 import javax.validation.constraints.NotEmpty;
 import javax.validation.constraints.NotNull;
 
 @Getter
 @JsonNaming(PropertyNamingStrategies.SnakeCaseStrategy.class)
+@AllArgsConstructor
+@NoArgsConstructor
 public class CreateCommentRequest {
     @NotNull
     private Long postId;
