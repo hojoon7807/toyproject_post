e6d0b28ae15de668996c93613dbabd38ae7f97ca
hojoon <ujuj0202@gmail.com>
2022-05-10 16:14:45 +0900

    refactor: x-www-urlencoded 방식의 메소드 추가

--- src/main/java/com/flab/posttoy/web/controller/UserController.java
+++ src/main/java/com/flab/posttoy/web/controller/UserController.java
@@ -5,8 +5,11 @@ import com.flab.posttoy.service.UserService;
 import com.flab.posttoy.web.dto.request.CreateUserRequest;
 import com.flab.posttoy.web.dto.response.UserResponseDTO;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
@@ -15,12 +18,21 @@ import javax.validation.Valid;
 
 @RestController
 @RequiredArgsConstructor
+@Slf4j
 public class UserController {
 
     private final UserService userService;
 
-    @PostMapping("/users")
+    @PostMapping(value = "/users", consumes = MediaType.APPLICATION_JSON_VALUE)
     public ResponseEntity<UserResponseDTO> userSave(@RequestBody @Valid CreateUserRequest createUserRequest){
+        log.info("json request. username: {}",createUserRequest.getUsername());
+        User user = userService.addUser(createUserRequest.toCommand());
+        return new ResponseEntity<>(UserResponseDTO.from(user), HttpStatus.CREATED);
+    }
+
+    @PostMapping(value = "/users", consumes = MediaType.APPLICATION_FORM_URLENCODED_VALUE)
+    public ResponseEntity<UserResponseDTO> userSaveFormRequest(@Valid CreateUserRequest createUserRequest){
+        log.info("form request. username: {}",createUserRequest.getUsername());
         User user = userService.addUser(createUserRequest.toCommand());
         return new ResponseEntity<>(UserResponseDTO.from(user), HttpStatus.CREATED);
     }
